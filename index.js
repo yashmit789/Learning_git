14661ba3d2b2ad2cb881a6b2fccfa9782940dadc
Yashmit Gupta <yashmitgupta576@gmail.com>
2026-03-02 23:15:16 +0530

    fifth commit

--- index.js
+++ index.js
@@ -9,5 +9,14 @@ function myage(){
 function hello(){
     console.log("Hello World");
 }   
+function add(a,b){
+    return a+b;
+}   
+function sub(a,b){
+    return a-b;
+}   
+function mul(a,b){
+    return a*b;
+}   
 myname();
 myage();
